588f008a8f48dc8aecaf39a7c710f977d8c2c941
Krishala Chaudhary <krishalac9@gmail.com>
2025-12-18 17:16:29 +0545

    init

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-console.log("hello world")
-console.log("bye world")
-console.log("hello")
+const express = require("express")
+const app = express()
+
+app.get("/", function(haha,hehe){
+    hehe.json({
+        name : "home page"
+    })
+})
+
+app.get("/about", function(req,res){
+    res.json({
+        address : "About page address",
+        age : 19,
+        name : "krishala"
+    })
+})
+
+
+app.listen(3000,function(){
+    console.log("server has started at port 3000")
+})
